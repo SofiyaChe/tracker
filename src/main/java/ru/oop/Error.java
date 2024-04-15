ada07132d96d2efcf0cff9efdd3e61e093106222
Admin <sofiya.chebotareva@bk.ru>
2024-04-15 16:28:30 +0200

    7.8 Конструктор

---
+++ src/main/java/ru/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.message = message;
+        this.status = status;
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Сообщение: " + message);
+        System.out.println("Статус: " + status);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 1, "Message1");
+        Error error2 = new Error(false, 2, "Message2");
+        error.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+    }
+}
