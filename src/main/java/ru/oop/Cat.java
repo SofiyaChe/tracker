ada31220df7dcf65438f28c589dbb958e17f0767
Admin <sofiya.chebotareva@bk.ru>
2024-04-15 08:20:06 +0200

    7.6 Поля объекта

---
+++ src/main/java/ru/oop/Cat.java
@@ -0,0 +1,29 @@
+package ru.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println( this.name + " ate " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("gav");
+        gav.show();
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("black");
+        black.show();
+    }
+}
