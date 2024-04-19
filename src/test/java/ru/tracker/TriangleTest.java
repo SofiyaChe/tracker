df5f949be1a1265a9f8d2447730e0ee68e20db30
Admin <sofiya.chebotareva@bk.ru>
2024-04-19 12:53:34 +0200

    7_14_Рефакторинг_Площадь_треугольника

---
+++ src/test/java/ru/tracker/TriangleTest.java
@@ -0,0 +1,33 @@
+package ru.tracker;
+
+import org.junit.jupiter.api.Test;
+import ru.oop.Point;
+import ru.oop.Triangle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+public class TriangleTest {
+    @Test
+    public void when00and40and04Then8() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = 8;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void whenNotExist() {
+        Point a = new Point(1, 0);
+        Point b = new Point(2, 0);
+        Point c = new Point(4, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = -1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+}
+
