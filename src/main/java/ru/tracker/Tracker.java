50bafc7cfb39a84f71c97717839ff306ad5b128b
Admin <sofiya.chebotareva@bk.ru>
2024-05-17 08:38:19 +0200

    9.7 Tracker - хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,30 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] newItems = new Item[size];
+        for (int index = 0; index < newItems.length; index++) {
+            Item item = items[index];
+            if (item != null) {
+                newItems[index] = item;
+            }
+        }
+        return Arrays.copyOf(newItems, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] newItems = new Item[size];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (items[index].getName().equals(key)) {
+                newItems[count] = item;
+                count++;
+            }
+        }
+        return Arrays.copyOf(newItems, count);
     }
 
     public Item findById(int id) {
