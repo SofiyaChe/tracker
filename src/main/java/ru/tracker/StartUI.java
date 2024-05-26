287004e6bd85050bde74b939681fc414a55c0efe
Admin <sofiya.chebotareva@bk.ru>
2024-05-26 20:59:27 +0200

    10.7 StartUI. Изменение заявки/2

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -40,8 +40,6 @@ public class StartUI {
                 }
             } else if (select == 6) {
                 run = false;
-            } else {
-                run = false;
             }
         }
     }
